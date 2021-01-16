3b6d2b5a65a8daf4ab68487c9554b59f394b05b0
Johannes Filter <hi@jfilter.de>
2021-01-16 21:56:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# ezra converter
+
+Converting data about right-wing incidents in Thuringia (_Thüringen_), Germany as monitored by the NGO [ezra](https://ezra.de/).
+
+We use an internal API to transform the data into another data format. So technically, we are not scraping.
+
+-   Website: <https://ezra.de/chronik/>
+-   API: <https://angstraeume.ezra.de/wp-json/ezra/v1/chronic">
+-   Data: <https://morph.io/rechtegewalt/ezra-converter>
+
+## Caveats
+
+1. Not consindering `endDates` for now
+2. Only using one location (the first in the array) if there are multiple.
+
+## Usage
+
+For local development:
+
+-   Install [poetry](https://python-poetry.org/)
+-   `poetry install`
+-   `poetry run python scraper.py`
+
+For Morph:
+
+-   `poetry export -f requirements.txt --output requirements.txt`
+-   commit the `requirements.txt`
+-   modify `runtime.txt`
+
+## Morph
+
+This is scraper runs on [morph.io](https://morph.io). To get started [see the documentation](https://morph.io/documentation).
+
+## License
+
+MIT
---
+++ scraper.py
@@ -0,0 +1,64 @@
+
+import dataset
+import get_retries
+from bs4 import BeautifulSoup
+from dateparser import parse
+
+db = dataset.connect("sqlite:///data.sqlite")
+
+tab_incidents = db["incidents"]
+tab_sources = db["sources"]
+tab_chronicles = db["chronicles"]
+
+
+tab_chronicles.insert(
+    {
+        "iso3166_1": "DE",
+        "iso3166_2": "DE-TH",
+        "chronicler_name": "ezra",
+        "chronicler_description": "ezra ist die Beratung für Betroffene rechter, rassistischer und antisemitischer Gewalt in Thüringen. Wir beraten, begleiten und unterstützen Menschen, die aus Motiven gruppenbezogener Menschenfeindlichkeit angegriffen werden – also deshalb, weil die Täter*innen sie einer von ihnen abgelehnten Personengruppe zuordnen. Daneben richtet sich unser Angebot auch an Angehörige von Betroffenen und an Zeug*innen.",
+        "chronicler_url": "https://ezra.de/",
+        "chronicle_source": "https://ezra.de/chronik/",
+    }
+)
+
+
+url = "https://angstraeume.ezra.de/wp-json/ezra/v1/chronic"
+
+json_data = get_retries.get(url, verbose=True, max_backoff=128).json()
+
+meta_motives = json_data["meta"]["motives"]
+meta_locations = json_data["meta"]["locations"]
+
+print(meta_motives)
+
+for x in json_data["entries"]:
+    city = x["locationDisplay"]
+
+    county = meta_locations[str(x["locations"][0])] if len(x["locations"]) > 0 else None
+
+    date = parse(x["startDisplay"], languages=["de"])
+    title = x["title"]
+    description = BeautifulSoup(x["content"], "lxml").get_text().strip()
+    rg_id = "ezra-" + str(x["id"])
+    motives = ", ".join([meta_motives[str(xx)] for xx in x["motives"]])
+
+    data = dict(
+        url="https://ezra.de/chronik/",
+        rg_id=rg_id,
+        date=date,
+        city=city,
+        county=county,
+        title=title,
+        description=description,
+        chronicler_name="ezra",
+    )
+
+    tab_incidents.upsert(data, ["rg_id"])
+
+    sources = x["sourceName"].replace("; ", ", ").split(",")
+    sources_data = [
+        dict(rg_id=rg_id, name=s.strip(), url=x["sourceUrl"]) for s in sources
+    ]
+    for s in sources_data:
+        tab_sources.upsert(s, ["rg_id"])
